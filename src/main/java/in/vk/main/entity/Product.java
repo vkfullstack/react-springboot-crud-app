a7b40bbac9d2009d976418b7d683615f2f3a1ad9
vkfullstack <vijaykhetre36@gmail.com>
2026-05-08 09:21:57 +0530

    feat: define JPA entities for product management

---
+++ src/main/java/in/vk/main/entity/Product.java
@@ -0,0 +1,57 @@
+package in.vk.main.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Integer id;
+	@Column
+	private String  productname;
+	@Column
+	private String description;
+	@Column
+	private String price;
+	@Column
+	private String status;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+
+}
