990589f232ecb332ac23339b9d2420dbfcb8c790
vkfullstack <vijaykhetre36@gmail.com>
2026-05-08 09:31:47 +0530

    feat: complete Product Management System backend implementation

---
+++ src/test/java/in/vk/main/ProductManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vk.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
